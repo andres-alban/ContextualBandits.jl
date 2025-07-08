aa1ef868c745f9a82bec097f3e93c9d6d4e75c47
andres-alban <a.alban@fs.de>
2025-07-08 09:28:19 +0200

    New allocation_current function for experiments

--- test/PolicyLinear_tests.jl
+++ test/PolicyLinear_tests.jl
@@ -26,8 +26,8 @@ using Test
     @test all(wpost .∈ Ref(1:n))
     @test ContextualBandits.policy_labeling(policy) == labeling
     rng = Xoshiro(1234)
-    @test ContextualBandits.allocationIndependent(policy, X, [W], X, [Y], rng) == w
-    @test ContextualBandits.implementationIndependent(policy, X, [W], X, [Y]) == wpost
+    @test allocation_current(policy, X, [W], X, [Y], rng) == w
+    @test implementation_current(policy, X, [W], X, [Y]) == wpost
 
     ContextualBandits.initialize!(policy, W, X, Y)
     theta_exp, Sigma_exp = BayesUpdateNormal(theta0, Sigma0, interact(W, n, X, labeling), Y, sample_std)
@@ -56,4 +56,33 @@ end
     @test ContextualBandits.allocation(policy, X, W, X, Y, rng) in 1:n
     @test all(ContextualBandits.implementation(policy, X, W, X, Y) .∈ Ref(1:3))
     @test ContextualBandits.policy_labeling(policy) == labeling
+end
+
+@testset "allocation_current" begin
+    n = 5
+    m = 2
+    sample_std = 1.0
+    labeling = vcat([true], falses(m * (n + 1) - 1))
+    theta0 = zeros(sum(labeling))
+    Sigma0 = diagm(ones(sum(labeling)))
+    policy = RandomPolicyLinear(n, m, theta0, Sigma0, sample_std, labeling)
+    ContextualBandits.initialize!(policy)
+
+
+    T = 4
+    X = randn(m, T)
+    Y = randn(T)
+    W1 = zeros(Int, T)
+    for t in 1:T
+        rng1 = Xoshiro(2024)
+        W1[t] = @test_nowarn allocation_current(policy, X[:, t], W1[1:t-1], X[:, 1:t-1], Y[1:t-1], rng1, true)
+    end
+
+    W2 = zeros(Int, T)
+    for t in 1:T
+        rng2 = Xoshiro(2024)
+        W2[t] = allocation_current(policy, X[:, t], W2[1:t-1], X[:, 1:t-1], Y[1:t-1], rng2, false)
+    end
+
+    @test W1 == W2
 end
